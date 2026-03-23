d63ee41d3faebb36f827aa94b1901cf70fcce490
Abhishek Agrawal <103879638+abhishe1906@users.noreply.github.com>
2026-03-23 17:25:00 +0530

    Update rag_pipeline.py

--- rag_pipeline.py
+++ rag_pipeline.py
@@ -127,7 +127,7 @@ Question:
 """
 
     response = client.chat.completions.create(
-        model="llama3-70b-8192",
+        model="mixtral-8x7b-32768",
         messages=[{"role": "user", "content": prompt}]
     )
 
